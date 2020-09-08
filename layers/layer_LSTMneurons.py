de8ffd80e333293666dc4d7a189fda6eeacfdc82
Marcin Dahlen <marcin.dahlen@syndatis.com>
2020-09-08 11:23:22 +0200

    LSTM layer tested and fixed

--- layers/layer_LSTMneurons.py
+++ layers/layer_LSTMneurons.py
@@ -9,22 +9,19 @@ from utils.utility import sigmoid, tanh
 
 
 class LSTMLayer(ILayer):
-    def __init__(self, in_shape: Tuple, out_shape: Tuple, size: int):
+    def __init__(self, in_shape: Tuple, size: int):
         self.outputs = None
-        self.previous_outputs = None
-        self.memories = None
+        self.previous_outputs = numpy.zeros(size)
+        self.memories = numpy.zeros(size)
         self.in_shape = in_shape
-        self.out_shape = out_shape
         self.size = size
-        self.weights = []
+        self.weights = self.init_weights(in_shape, size)
 
     def forward_pass(self, input) -> numpy.ndarray:
         input = tf.reshape(input, self.in_shape)
         input = numpy.append(input, [1])  # append bias
-        if not self.previous_outputs:
-            input = numpy.append(input, [0])  # append neutral 0 if no previous output present
-        else:
-            input = numpy.append(input, self.previous_outputs)
+        input = numpy.append(input, [self.previous_outputs])
+
         input_gates = [numpy.matmul(input, self.weights[w][0]) for w in range(self.size)]
         input_gates = [numpy.sum(e) for e in input_gates]
         input_gates = [sigmoid(s) for s in input_gates]
@@ -34,9 +31,11 @@ class LSTMLayer(ILayer):
         memory_gates = [numpy.matmul(input, self.weights[w][2]) for w in range(self.size)]
         memory_gates = [numpy.sum(e) for e in memory_gates]
         memory_gates = [numpy.delete(e, -1) for e in memory_gates]  # previous output not relevant in this gate
-        memory_gates = [tanh(memory_gates) for s in forget_gates]
+        memory_gates = [tanh(n) for n in memory_gates]
+
         self.memories = [forget_gates[cell] * self.memories[cell] +
                          input_gates[cell] * memory_gates[cell] for cell in range(self.size)]
+
         output_gates = [numpy.matmul(input, self.weights[w][3]) for w in range(self.size)]
         output_gates = [numpy.sum(e) for e in output_gates]
         output_gates = [numpy.delete(e, -1) for e in output_gates]  # previous output not relevant in this gate
@@ -64,5 +63,6 @@ class LSTMLayer(ILayer):
 
     def init_weights(self, in_shape: Tuple, size: int) -> numpy.ndarray:
         self.in_shape = reduce(lambda x, y: x * y, in_shape)
-        size = (size, 4, self.in_shape + 1 + 1)  # (no_of_neurons, no_of_gates, no_of_weights + bias + previous_out)
+        # size = (no_of_neurons, no_of_gates, no_of_weights + bias + previous_outputs)
+        size = (size, 4, self.in_shape + 1 + 1 * size)
         return numpy.random.normal(loc=0, scale=0.25, size=size)
---
+++ tests/testLSTMLayer.py
@@ -0,0 +1,27 @@
+import numpy
+
+from layers.layer_LSTMneurons import LSTMLayer
+
+
+class TestLSTMLayer:
+    def test_layer(self):
+        data_1d = numpy.random.rand(15)
+        data_2d = numpy.random.rand(15, 15)
+
+        print(data_1d)
+        print(data_2d)
+
+        LSTM_1d_layer = LSTMLayer((15,), 4)
+        LSTM_2d_layer = LSTMLayer((15, 15), 4)
+
+        print("")
+        print("")
+        output = LSTM_1d_layer.forward_pass(data_1d)
+        print(output)
+        assert numpy.shape(output) == (4,)
+        print("")
+        print("")
+        output = LSTM_2d_layer.forward_pass(data_2d)
+        print(output)
+
+        assert numpy.shape(output) == (4,)
